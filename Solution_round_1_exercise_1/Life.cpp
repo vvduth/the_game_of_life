2f71b0714700b304de0a24621f86800baf35d598
vvduth <ducthai060501@gmail.com>
2021-09-08 13:28:07 +0300

    Task 3

--- Solution_round_1_exercise_1/Life.cpp
+++ Solution_round_1_exercise_1/Life.cpp
@@ -11,15 +11,92 @@ Post: The number of living neighbors of the specified cell is returned.
 {
     int i, j;
     int count = 0;
-    for (i = row - 1; i <= row + 1; i++)
-        for (j = col - 1; j <= col + 1; j++)
-            count += grid[i][j];  //  Increase the count if neighbor is alive.
-    count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
-    return count;
+    if (row > 0 && col > 0 && row < maxrow -1 && col < maxcol -1 ) {
+        for (i = row - 1; i <= row + 1; i++)
+            for (j = col - 1; j <= col + 1; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    } //default
+    else if (col == 0 && row > 0 && row < maxrow - 1) {  //col = 0
+        for (i = row - 1; i <= row + 1; i++)
+            for (j = col; j <= col + 1; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+
+    } else if (col < maxcol - 1  && col > 0 && row == 0 ) { // row = 0
+        for (i = row ; i <= row + 1; i++)
+            for (j = col - 1; j <= col + 1; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+
+    } else if (col == maxcol -1 && row > 0 && row < maxrow - 1){ //col = hedge
+        for (i = row - 1; i <= row + 1; i++)
+            for (j = col - 1; j <= col ; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    }
+    else if (row == maxrow - 1 && col > 0 && col < maxrow - 1) { //row = hedge
+        for (i = row - 1; i <= row ; i++)
+            for (j = col - 1; j <= col + 1; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    }
+    else if (row == 0 && col == 0 ) { //both = 0
+        for (i = row ; i <= row + 1; i++)
+            for (j = col ; j <= col + 1; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    }
+    else if (row == maxrow - 1 && col == maxcol - 1) { //both = hedge
+        for (i = row-1; i <= row ; i++)
+            for (j = col-1; j <= col ; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    }
+    else if (row == 0 && col == maxcol - 1) { //row = 0 and col = hedge
+        for (i = row ; i <= row + 1; i++)
+            for (j = col - 1; j <= col; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    }
+    else if (row == maxrow -1 && col == 0) { //row = hegde and col = 0
+        for (i = row -1 ; i <= row ; i++)
+            for (j = col ; j <= col + 1; j++)
+                count += grid[i][j];  //  Increase the count if neighbor is alive.
+        count -= grid[row][col]; //  Reduce count, since cell is not its own neighbor.
+
+
+        return count;
+    }
+    
 }
 
 void Life::setMaxRow(int maxrow) {
-    this -> maxrow = maxrow;
+    this->maxrow = maxrow;
 }
 void Life::setMaxCol(int maxcol) {
     this->maxcol = maxcol;
@@ -41,13 +118,13 @@ Post: The Life object contains the next generation of configuration.
     int row, col;
     //int new_grid[maxrow + 2][maxcol + 2];
     int** new_grid = new int* [maxrow + 2];
-    for (int i = 0; i < maxrow + 2; i++) {
-        new_grid[i] = new int[maxcol + 2];
-       
+    for (int i = 0; i < maxrow ; i++) {
+        new_grid[i] = new int[maxcol];
+
     }
 
-    for (row = 1; row <= maxrow; row++)
-        for (col = 1; col <= maxcol; col++)
+    for (row = 0; row < maxrow; row++)
+        for (col = 0; col < maxcol; col++)
             switch (neighbor_count(row, col)) {
             case 2:
                 new_grid[row][col] = grid[row][col];  //  Status stays the same.
@@ -59,11 +136,11 @@ Post: The Life object contains the next generation of configuration.
                 new_grid[row][col] = 0;                //  Cell is now dead.
             }
 
-    for (row = 1; row <= maxrow; row++)
-        for (col = 1; col <= maxcol; col++)
+    for (row = 0; row < maxrow; row++)
+        for (col = 0; col < maxcol; col++)
             grid[row][col] = new_grid[row][col];
 
-    for (int i = 0; i < maxrow + 2; i++) {
+    for (int i = 0; i < maxrow ; i++) {
         delete[] new_grid[i];
     }
     delete[] new_grid;
@@ -78,26 +155,26 @@ Post: The Life object contains a configuration specified by the user.
 */
 
 {
-    
+
     int row, col;
-    grid = new int* [maxrow + 2];
-    for (int i = 0; i < maxrow + 2; i++) {
-        grid[i] = new int[maxcol + 2];
-        
+    grid = new int* [maxrow];
+    for (int i = 0; i < maxrow ; i++) {
+        grid[i] = new int[maxcol];
+
     }
 
-    for (row = 0; row <= maxrow + 1; row++)
-        for (col = 0; col <= maxcol + 1; col++)
+    for (row = 0; row < maxrow ; row++)
+        for (col = 0; col < maxcol ; col++)
             grid[row][col] = 0;
-    
+
     cout << "List the coordinates for living cells." << endl;
     cout << "Terminate the list with the special pair -1 -1" << endl;
     cin >> row >> col;
 
     while (row != -1 || col != -1) {
-        if (row >= 1 && row <= maxrow)
-            if (col >= 1 && col <= maxcol)
-                grid[row][col] = 1;
+        if (row - 1 >= 0 && row - 1 <= maxrow)
+            if (col - 1 >= 0 && col - 1 <= maxcol)
+                grid[row - 1 ][col - 1] = 1;
             else
                 cout << "Column " << col << " is out of range." << endl;
         else
@@ -114,10 +191,10 @@ Post: The configuration is written for the user.
 */
 
 {
-   int row, col;
+    int row, col;
     cout << "\nThe current Life configuration is:" << endl;
-    for (row = 1; row <= maxrow; row++) {
-        for (col = 1; col <= maxcol; col++)
+    for (row = 0; row < maxrow; row++) {
+        for (col = 0; col < maxcol; col++)
             if (grid[row][col] == 1) cout << '*';
             else cout << '.';
         cout << endl;
